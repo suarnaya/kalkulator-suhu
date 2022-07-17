df6f9df40bff11bc299d25f5cbfd006a82700566
suarnaya <kedusashram99@gmail.com>
2022-07-17 21:23:27 +0800

    menambahkan semua jenis suhu

--- script.js
+++ script.js
@@ -1,24 +1,66 @@
-// program sederhana konversi suhu celcius ke reamur dan sebaliknya
+const celcius = document.getElementById('celcius')
+const fahrenheit = document.getElementById('fahrenheit')
+const kelvin = document.getElementById('kelvin')
+const reamur = document.getElementById('reamur')
 
-// tentukan nilai varible, letakan paling atas
-let nilai = 0;
-let bool = true;
+let suhu = function (type) {
+  let toReamur,
+    toCelcius,
+    toKelvin,
+    toFahrenheit = 0
 
-bool = confirm("pilih konversi: OK = celcius>reamur. CANCEL = reamur>celcius"); // buat pengkondisian, apakah mau konversi celcius / reamur
-if(bool === true) {
-	// masukan nilai lewat prompt yang sudah di konversi ke angka, lakukan pengkondisian
-	nilai = parseInt(prompt("masukan derajat suhu celcius: "));
-} else {
-	nilai = parseInt(prompt("masukan derajat suhu reamur: "));
+  printCelcius = () => (celcius.value = parseFloat(toCelcius.toFixed(2)))
+  printReamur = () => (reamur.value = parseFloat(toReamur.toFixed(2)))
+  printFahrenheit = () =>
+    (fahrenheit.value = parseFloat(toFahrenheit.toFixed(2)))
+  printKelvin = () => (kelvin.value = parseFloat(toKelvin.toFixed(2)))
+
+  if (type == celcius) {
+    toFahrenheit = (parseFloat(type.value) * 9) / 5 + 32
+    toReamur = (parseFloat(type.value) / 5) * 4
+    toKelvin = parseFloat(type.value) + 273.15
+    printReamur()
+    printKelvin()
+    printFahrenheit()
+    //////
+  } else if (type == fahrenheit) {
+    toCelcius = ((parseFloat(type.value) - 32) * 5) / 9
+    toKelvin = ((parseFloat(type.value) - 32) * 5) / 9 + 273.15
+    toReamur = ((parseFloat(type.value) - 32) * 4) / 9
+
+    printReamur()
+    printKelvin()
+    printCelcius()
+    //////
+  } else if (type == reamur) {
+    toFahrenheit = (parseFloat(type.value) * 9) / 4 + 32
+    toCelcius = (parseFloat(type.value) * 5) / 4
+    toKelvin = (parseFloat(type.value) * 5) / 4 + 273.15
+
+    printKelvin()
+    printFahrenheit()
+    printCelcius()
+    //////
+  } else if (type == kelvin) {
+    toCelcius = parseFloat(type.value) - 273.15
+    toReamur = ((parseFloat(type.value) - 273.15) * 4) / 5
+    toFahrenheit = ((parseFloat(type.value) - 273.15) * 4) / 5 + 32
+
+    printReamur()
+    printFahrenheit()
+    printCelcius()
+  }
 }
 
-let hasil = function() {
-	// lakukan pengkondisian apakah konversi dari celcius apa reamur
-	if(bool === true) {
-		return nilai * 4 / 5;
-	} else {
-		return nilai * 5 / 4;
-	}
+celcius.oninput = () => {
+  suhu(celcius)
 }
-// panggil function tadi
-document.getElementById("demo").innerHTML = hasil();	
+fahrenheit.oninput = () => {
+  suhu(fahrenheit)
+}
+reamur.oninput = () => {
+  suhu(reamur)
+}
+kelvin.oninput = () => {
+  suhu(kelvin)
+}
